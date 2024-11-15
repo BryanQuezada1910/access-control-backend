18d8f133906364f0d1ad9a8f82bfa3e20844c77f
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-11-14 23:29:39 -0600

    Create NfcCard.js

---
+++ src/models/NfcCard.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const NfcCardSchema = new mongoose.Schema({
+  cardId: { type: String, required: true, unique: true },
+  isAsigned: { type: Boolean, default: false },
+  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("NfcCard", NfcCardSchema);
