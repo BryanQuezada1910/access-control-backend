e04bd01ba407be08c200a0cf13e94d986fd84ef2
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-11-19 02:32:46 -0600

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  uuidCard: {
+  nfcCard: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "NfcCard",
   },
