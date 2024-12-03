6a44d8ad5d537de27d5ed58a807fea2fd5f6b4cf
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-12-03 17:39:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import connectDB from "./src/config/database.js";
 dotenv.config();
 
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL, "http://localhost:9000"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
